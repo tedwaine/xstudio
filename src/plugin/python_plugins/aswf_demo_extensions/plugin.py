2f6c66a6e6885352bc52d9552c09236e8b59e976
Ted Waine <ted.waine@gmail.com>
2026-05-29 17:47:42 +0100

    Adding viewport transform reset example

--- src/plugin/python_plugins/aswf_demo_extensions/plugin.py
+++ src/plugin/python_plugins/aswf_demo_extensions/plugin.py
@@ -3,7 +3,7 @@
 
 from xstudio.plugin import PluginBase
 from xstudio.core import get_global_playhead_events_atom, event_atom, viewport_atom, viewport_playhead_atom, name_atom
-from xstudio.core import transform_matrix_atom, viewport_pan_atom, viewport_scale_atom
+from xstudio.core import transform_matrix_atom, viewport_pan_atom, viewport_scale_atom, V2f
 from xstudio.api.auxiliary import ActorConnection
 from xstudio.api.intrinsic import Viewport
 from xstudio.api.module import ModuleBase
@@ -31,9 +31,17 @@ class ViewportLocal(Viewport):
     def pan(self):
         return self.connection.request_receive(self.remote, viewport_pan_atom())[0]
 
+    @pan.setter
+    def pan(self, value):
+        self.connection.send(self.remote, viewport_pan_atom(), value)
+
     @property
     def scale(self):
         return self.connection.request_receive(self.remote, viewport_scale_atom())[0]
+
+    @scale.setter
+    def scale(self, value):
+        self.connection.request_receive(self.remote, viewport_scale_atom(), value)[0]
 
 # Similar to the ViewportLocal class above, we can also make a class that attaches
 # to a playhead in the backend. This lets us get attribute updates from the 
@@ -60,6 +68,18 @@ class ASWFExtensionPlugin(PluginBase):
             connection,
             name="DemoPluginPython"
             )
+
+        self.menu_id = self.insert_menu_item(
+            menu_model_name="main menu bar",
+            menu_text="Reset Pan/Zoom",
+            menu_path="ASWF Demo Extensions",
+            menu_item_position=100.0,
+            callback=self.reset_pan_zoom)
+
+        self.set_submenu_position(
+            menu_model_name="main menu bar",
+            submenu_path="ASWF Demo Extensions",
+            menu_item_position=10.0)
 
         # Subscribe to global playhead events. We will get various
         # events relating to the creation of viewports, viewports
@@ -96,6 +116,10 @@ class ASWFExtensionPlugin(PluginBase):
             print ("Transform matrix changed ", str(event[4]))
             print ("PAN ", self.viewport.pan, "SCALE ", self.viewport.scale)
 
+    def reset_pan_zoom(self):
+        if self.viewport:
+            self.viewport.pan = V2f(0.0, 0.0)
+            self.viewport.scale = 1.0
 
 # This method is required by xSTUDIO
 def create_plugin_instance(connection):
